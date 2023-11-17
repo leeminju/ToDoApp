ee6084d929c2903aef73449b2119c36666a1c3b6
leeminju <leemj4090@naver.com>
2023-11-17 21:30:53 +0900

    api requestMapping api/comment로 통합!

--- src/main/java/com/sparta/board2/controller/CommentController.java
+++ src/main/java/com/sparta/board2/controller/CommentController.java
@@ -14,27 +14,27 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api")
+@RequestMapping("/api/comment")
 public class CommentController {
 
     private final CommentService commentService;
 
-    @PostMapping("/comment/{post_id}")
+    @PostMapping("/{post_id}")
     public CommentResponseDto createComment(@PathVariable Long post_id, @RequestBody CommentRequestDto requestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
         return commentService.createComment(post_id, requestDto, userDetails.getUser());
     }
 
-    @GetMapping("/comment/{post_id}")
+    @GetMapping("/{post_id}")
     public List<CommentResponseDto> getComments(@PathVariable Long post_id) {
         return commentService.getComments(post_id);
     }
 
-    @PutMapping("/comment/{comment_id}")
+    @PutMapping("/{comment_id}")
     public ResponseEntity<?> updateComment(@PathVariable Long comment_id, @RequestBody CommentRequestDto requestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
         return commentService.updateComment(comment_id, requestDto, userDetails.getUser());
     }
 
-    @DeleteMapping("/comment/{comment_id}")
+    @DeleteMapping("/{comment_id}")
     public ResponseEntity<?> deleteComment(@PathVariable Long comment_id, @AuthenticationPrincipal UserDetailsImpl userDetails) {
         return commentService.deleteComment(comment_id, userDetails.getUser());
     }
