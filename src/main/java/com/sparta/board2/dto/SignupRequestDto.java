bf221c811f36b69b879d894797afb8ceb23c9c9d
leeminju <leemj4090@naver.com>
2023-12-02 23:23:41 +0900

    Signup @Pattern 변경

--- src/main/java/com/sparta/board2/dto/SignupRequestDto.java
+++ src/main/java/com/sparta/board2/dto/SignupRequestDto.java
@@ -12,12 +12,12 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class SignupRequestDto {
     @NotBlank(message = "username은 공백일 수 없습니다.")
-    @Pattern(regexp = "^(?=.*[a-z0-9]+)(?=.*[a-z0-9])[a-z0-9]{4,10}$", message = "username은 영어 소문자, 숫자 조합만 가능합니다.")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*\\d)[a-z\\d]+$", message = "username은 영어 소문자, 숫자 조합만 가능합니다.")
     @Size(min = 4, max = 10, message = "username은 4자 이상 10자 이하로 입력해주세요")
     private String username;
 
     @NotBlank(message = "password는 공백일 수 없습니다.")
-    @Pattern(regexp = "^(?=.*[a-zA-Z0-9]+)(?=.*[a-zA-Z0-9])[a-zA-Z0-9]{8,15}$", message = "password는 영어 대문자, 소문자, 숫자 조합만 가능합니다.")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]+$", message = "password는 영어 대문자, 소문자, 숫자 조합만 가능합니다.")
     @Size(min = 8, max = 15, message = "password는 8자 이상 15자 이하로 입력해주세요")
     private String password;
 }
