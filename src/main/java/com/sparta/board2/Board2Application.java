ba1ed702ec168b0c5a513b8c39a9790a70a66c01
leeminju <leemj4090@naver.com>
2023-12-03 01:56:19 +0900

    JpaConfig 추가

--- src/main/java/com/sparta/board2/Board2Application.java
+++ src/main/java/com/sparta/board2/Board2Application.java
@@ -2,9 +2,8 @@ package com.sparta.board2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
+
 @SpringBootApplication
 public class Board2Application {
 
